67d90643d02f8cf5c75ce98a6b1517f9b03eef78
deborahdelimaa <deborahftlima@gmail.com>
2023-02-08 21:15:39 +0000

    lab fixed

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
-const app  = express()
+const { response } = require("express");
 const express = require("express");
+
+const app  = express()
 
 app.use(express.static("public"));
 
 app.get("/", (request, response)=> {
     console.log(__dirname);
     response.sendFile(__dirname + "/view/home.html")
-});
-app.listen(3000, ()=> console.log("Server running on port 3000")) 
+}); 
+
+app.get("/about", (request, response) => {
+
+    response.sendFile(__dirname + "/view/about.html");
+  
+  });
+  
+  
+  
+  app.get("/photo", (request, response) => {
+  
+    response.sendFile(__dirname + "/view/photo.html");
+  
+  });
+  
+  
+  
+  app.get("/works", (request, response) => {
+  
+    response.sendFile(__dirname + "/view/works.html");
+  
+  });
+
+
+app.listen(3000, ()=> console.log("Server running on port 3000"))
